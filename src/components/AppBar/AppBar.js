c320cae0574576a810b8554f1859cd2be5fc134e
Dmytro-Avramenko <himera33a@gmail.com>
2023-03-29 23:00:16 +0300

    more

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,7 +2,7 @@ import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks';
-import s from '../AppBar/AppBar.module.css';
+import s from '../../components/AppBar/AppBar.module.css';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
